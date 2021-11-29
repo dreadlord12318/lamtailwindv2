d982edb949dcf131c1086c1df553067403e702f4
= <dreadlord12318@gmail.com>
2021-11-29 09:04:20 +0800

    text changes

--- resources/lang/vendor/cookie-consent/en/texts.php
+++ resources/lang/vendor/cookie-consent/en/texts.php
@@ -2,5 +2,5 @@
 
 return [
     'message' => 'Our website uses cookies to give you the best and most relevant experience. By clickin on allow, you give consent to the use of cookies as per our <a href = "/privacy">privacy policy</a>',
-    'agree' => 'Allow cookies',
+    'agree' => 'Allow',
 ];
