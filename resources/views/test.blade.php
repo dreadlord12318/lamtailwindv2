6ba769c009b914e75bc4286dedd505415ad07aa1
= <dreadlord12318@gmail.com>
2022-01-18 14:12:03 +0800

    Removed Calendly for staging

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -155,8 +155,8 @@
 </div>
 
 <!-- Calendly inline widget begin -->
-<div class="calendly-inline-widget" data-url="https://calendly.com/lambent_co/15min" style="min-width:320px;height:630px;"></div>
-<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+<!-- <div class="calendly-inline-widget" data-url="https://calendly.com/lambent_co/15min" style="min-width:320px;height:630px;"></div>
+<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script> -->
 <!-- Calendly inline widget end -->
 
 
