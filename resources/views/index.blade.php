d1574b022b41655a65352259a5cd462af881f3a6
= <dreadlord12318@gmail.com>
2022-01-27 13:12:14 +0800

    Home Hero and CTA section update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,7 +47,63 @@
 		          <div class="mt-10 sm:mt-12">
               <img class="w-44 mt-3 md:center " src="images/LAMB - Proof Image - Clutch Review.svg" alt="">
               <!--  -->
-               
+              <h2 class = "mt-3  font-copy text-2xl font-bold font-normal-400 tracking-normal leading-relaxed text-left text-white normal-case sm:mt-5">We'll prove it: give us that task you've been putting off and we'll do it — for free.</h3>
+              @if (session('status'))
+                  <div role="alert" class = "w-full">
+                    <div class="bg-lambright-600 mt-2 text-white font-bold top-10 rounded-t px-4 py-3">
+                      Success
+                    </div>
+                    <div class="border border-t-0 border-lambright-600 rounded-b bg-white px-4 py-3 text-newcolor-200">
+                      <p>  {{ session('status') }}</p>
+                    </div>
+                  </div>
+              @endif
+              <div class="mt-5">
+                <form action="/contact" method="POST" class="sm:max-w-2xl sm:mx-auto lg:mx-0">
+                {{ csrf_field()}}
+                  <div class="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+                      <div>
+                <div class="mt-1">
+                  <input type="text" placeholder="First Name" name="first_name" id="first_name" value = "{{old('first_name')}}" autocomplete="given-name" class="block w-full py-3.5 px-4 font-concourset3 text-base font-normal-400 tracking-normal leading-relaxed normal-case shadow-sm s focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md">
+                </div>
+                @if($errors->has('first_name'))
+          <div class="flex items-center bg-blue-500 text-white text-sm font-bold my-2 rounded-md px-4 py-3" role="alert">
+  <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
+  <p>{{ $errors->first('first_name') }}</p>
+</div>
+          @endif
+              </div>
+              <div>
+                <div class="mt-1">
+                  <input type="text" placeholder="Last Name" name="last_name" id="last_name" value = "{{old('last_name')}}" autocomplete="family-name" class="block w-full font-concourset3 text-base font-normal-400 tracking-normal leading-relaxed normal-case  py-3.5 px-4 shadow-sm  focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md">
+                </div>
+                @if($errors->has('last_name'))
+          <div class="flex items-center bg-blue-500 text-white text-sm font-bold my-2 rounded-md px-4 py-3" role="alert">
+  <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
+  <p>{{ $errors->first('last_name') }}</p>
+</div>
+          @endif
+              </div>
+              <div class="sm:col-span-2">
+                <div class="mt-1">
+                  <input id="email" placeholder="Email"  name="email" type="email" value = "{{old('email')}}" autocomplete="email" class="block w-full font-concourset3 text-base font-normal-400 tracking-normal leading-relaxed normal-case  py-3.5 px-4 shadow-sm  focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md">
+                </div>
+                @if($errors->has('email'))
+              <div class="flex items-center bg-blue-500 text-white text-sm font-bold my-2 rounded-md px-4 py-3" role="alert">
+            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
+            <p>{{ $errors->first('email') }}</p>
+          </div>
+          @endif
+              </div>
+              <div class="sm:col-span-2">
+                    <div class="mt-1">
+                      <button type="submit" class="block w-full py-3  font-concourset6 text-lg capitalize font-normal tracking-normal leading-normal align-middle px-5 rounded-md shadow bg-newgrowth-300 text-white hover:bg-newgrowth-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-300 focus:ring-offset-gray-900">Let's Talk</button>
+                    </div>
+                </div>
+                  </div>
+    
+                </form>
+                </div>
               </div>
             </div>
           
@@ -56,7 +112,7 @@
           <div class=" bg-blend-overlay -mb-16 -top-40 sm:-mb-48 lg:m-0 lg:relative">
             <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
               <!-- Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ -->
-              <img class="ml-24  w-11/12 lg:absolute lg:inset-y-0 lg:left " src="images/LAMB-Site-Image-Home_HeaderIso.svg" alt="">
+              <img class="lg:ml-24  w-11/12 lg:absolute lg:top-40 lg:left " src="images/LAMB-Site-Image-Home_HeaderIso.svg" alt="">
               <!-- <div class="absolute inset-0 bg-lambright-600 mix-blend-lighten" aria-hidden="true"></div> -->
             </div>
           </div>
@@ -69,6 +125,35 @@
 
 <!-- Split with image -->
 
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="relative bg-newcolor-300">
+  <div class="h-56 bg-newcolor-300 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
+    <img class="w-full h-full object-cover" src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply" alt="">
+  </div>
+  <div class="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+    <div class="md:ml-auto md:w-1/2 md:pl-10">
+      <h2 class="font-concourset6 text-base font-normal tracking-normal leading-normal uppercase text-white">
+        One Free Task
+      </h2>
+      <p class="mt-2 text-white font-concourset7 text-3xl font-normal tracking-normal leading-snug capitalize">
+      We'll Eat Your Frog
+      </p>
+      <p class="mt-3 text-lg text-white">
+        Mark Twain famously prescribed frog-eating to combat procrastination.<br> What task do you daydream of doing? You
+        have nothing to lose but your frogs, so let us show you how we get the job done.
+      </p>
+      <div class="mt-8">
+        <div class="inline-flex rounded-md shadow">
+          <a href="#" class="inline-flex items-center bg-lamblue-600 justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white  hover:bg-lamblue-400">
+            Schedule a Call Today!
+            <!-- Heroicon name: solid/external-link -->
+       
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 
