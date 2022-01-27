c404434ccbebe9e217a63f6f1b7bac09e02249b6
= <dreadlord12318@gmail.com>
2022-01-27 13:27:43 +0800

    Frog Image

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -128,7 +128,7 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <div class="relative bg-newcolor-300">
   <div class="h-56 bg-newcolor-300 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
-    <img class="w-full h-full object-cover" src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply" alt="">
+    <img class="w-full h-full object-cover" src="images/zeteny-lisztes-ErMciS1yBs0-unsplash.jpg" alt="">
   </div>
   <div class="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
     <div class="md:ml-auto md:w-1/2 md:pl-10">
