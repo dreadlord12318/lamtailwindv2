cfd04c2b4e7246e4530b27c87777eedffaa27eb0
= <dreadlord12318@gmail.com>
2022-02-03 09:18:21 +0800

    Contact Copy Updates

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,8 +10,8 @@
     <div class="absolute inset-0 bg-lambright-500 mix-blend-multiply" aria-hidden="true"></div>
   </div>
   <div class="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
-    <h1 class="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">A Little More Conversation</h1>
-    <p class="mt-6 text-xl text-indigo-100 max-w-3xl">Mattis amet hendrerit dolor, quisque lorem pharetra. Pellentesque lacus nisi urna, arcu sociis eu. Orci vel lectus nisl eget eget ut consectetur. Sit justo viverra non adipisicing elit distinctio.</p>
+    <h1 class="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">We Thrive on Conversation</h1>
+    <p class="mt-6 text-xl text-indigo-100 max-w-3xl">What are you doing and how can we help you? We geek out on speaking with you about what’s getting you up in the morning and how we can caffeinate your workflow. Everything begins with a conversation. Let’s do this!</p>
   </div>
 </div>
 
