e93e31c402985ded17d58c986e8a13056a4e983a
= <dreadlord12318@gmail.com>
2022-02-23 09:23:41 +0800

    Error Fix Minor

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -154,7 +154,7 @@
             <div class="sm:col-span-2">
              
               <div class="mt-1">
-                <input type="text"  name="subject" id="subject" placeholder = "Subject" value="I want Free Magic class="py-3 px-4 block w-full shadow-sm font-concourset3 text-base font-normal text-newcolor-200 tracking-normal leading-relaxed normal-case focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" readonly>
+                <input type="text"  name="subject" id="subject" placeholder = "Subject" value="I want Free Magic" class="py-3 px-4 block w-full shadow-sm font-concourset3 text-base font-normal text-newcolor-200 tracking-normal leading-relaxed normal-case focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" readonly>
               </div>
             </div>
             <div class="sm:col-span-2">
