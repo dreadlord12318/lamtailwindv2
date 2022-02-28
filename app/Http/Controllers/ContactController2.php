a5a1c3e9c64cc8b84abc1c078174602acb7545f5
= <dreadlord12318@gmail.com>
2022-02-28 11:10:08 +0800

    recaptcha validation test 3

--- app/Http/Controllers/ContactController2.php
+++ app/Http/Controllers/ContactController2.php
@@ -19,13 +19,19 @@ class ContactController2 extends Controller
         $request->validate([
             'g-recaptcha-response' => 'required|string',
         ]);
+
+        $response = Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
+            'secret' => config('services.recaptcha.secret_key'),
+            'response' => $request->get('g-recaptcha-response'),
+            'remoteip' => $request->getClientIp(),
+        ]);
         
       
             // Mail::to('a.t.169630176.u-25541662.f7cc6750-c6df-4527-a207-1b3c396f25ae@tasks.clickup.com')
             // ->cc('info@lambent.co')
             // ->send(new Contact2( $request));
 
-        Mail::to('janly@lumikha.c')->send(new Contact2( $request));
+        Mail::to('janly@lumikha.co')->send(new Contact2( $request));
 
 
        return back()->with('status', 'Your Message has been received');
