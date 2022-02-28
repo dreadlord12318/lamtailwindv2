1dfb1f841c76ed8a215ee01b31bfc28d7a20b2a5
= <dreadlord12318@gmail.com>
2022-02-28 11:30:07 +0800

    recaptcha test 4

--- app/Http/Controllers/ContactController2.php
+++ app/Http/Controllers/ContactController2.php
@@ -16,10 +16,30 @@ class ContactController2 extends Controller
     public function mail(ContactRequest2 $request)
     {
         
+        // $request->validate([
+        //     'g-recaptcha-response' => function ($attribute, $value, $fail) {
+        //         $secret = config('services.recaptcha.secret_key');
+        //         $response = $request->get('g-recaptcha-response');
+        //         $remoteip = $request->getClientIp();
+        //         $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$response&remoteip=$remoteip";
+        //     }
+        // ]);
+
         $request->validate([
             'g-recaptcha-response' => 'required|string',
         ]);
 
+        $response = Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$response&remoteip=$remoteip', [
+            'secret' => config('services.recaptcha.secret_key'),
+            'response' => $request->get('g-recaptcha-response'),
+            'remoteip' => $request->getClientIp(),
+        ]);
+
+        if (! $response->json('success')) {
+            throw ValidationException::withMessages(['g-recaptcha-response' => 'Error verifying reCAPTCHA, please try again.']);
+        }
+       
+        
       
             // Mail::to('a.t.169630176.u-25541662.f7cc6750-c6df-4527-a207-1b3c396f25ae@tasks.clickup.com')
             // ->cc('info@lambent.co')
