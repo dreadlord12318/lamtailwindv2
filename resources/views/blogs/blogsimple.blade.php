1bb3ac422beb7ad4799d48da8bbb22df951cf134
= <dreadlord12318@gmail.com>
2022-03-15 15:12:57 +0800

    Fix blog simple

--- resources/views/blogs/blogsimple.blade.php
+++ resources/views/blogs/blogsimple.blade.php
@@ -69,12 +69,12 @@
       </div>
     </div>
     <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-     
+<!--      
       <figure>
         <img class="w-full rounded-lg" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3" alt="" width="1310" height="873">
         <figcaption>Sagittis scelerisque nulla cursus in enim consectetur quam.</figcaption>
       </figure>
-     
+      -->
     </div>
   </div>
 </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.blog')
 
 @section('title', 'Lambent: We create stories here')
 
