a5c36570af4e7a60bc717a6a23490e8f3eda611d
= <dreadlord12318@gmail.com>
2022-04-04 17:28:05 +0800

    Updated copy 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -17,7 +17,7 @@
   <div class="max-w-7xl mx-auto px-4 py-96 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-96">
     <p class="text-sm font-semibold text-white uppercase tracking-wide">404 error</p>
     <h1 class="mt-2 text-4xl font-extrabold text-white tracking-tight sm:text-5xl">Uh oh! I think you’re lost.</h1>
-    <p class="mt-2 text-lg font-medium text-white ">Let us know when you want to schedule your site visit. </p>
+    <p class="mt-2 text-lg font-medium text-white ">But feel free to enjoy one of our awe-inspiring Philippines sunsets. Let us know when you want to <br> schedule your site visit.</p>
     <div class="mt-6">
       <a href="/" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-black text-opacity-75 bg-white bg-opacity-75 sm:bg-opacity-25 sm:hover:bg-opacity-50"> Go back home </a>
     </div>
