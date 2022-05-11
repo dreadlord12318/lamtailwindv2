8c91a153555d3d81fd674351911bff5d67585296
= <dreadlord12318@gmail.com>
2022-05-11 12:46:57 +0800

    Added Images

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -11,7 +11,7 @@
 
 <div class="relative top-16 pb-20 bg-blue-700">
   <div class="absolute inset-0">
-    <img class="w-full h-full object-cover" src="images/Lambent-Team-Values.jpg" alt="Lambent’s values inform our work.">
+    <img class="w-full h-full object-cover" src="images/LAMB-Site-Image-Header_Services.jpg" alt="Lambent’s values inform our work.">
     <div class="absolute inset-0 h-full bg-blue-700 mix-blend-multiply" aria-hidden="true"></div>
   </div>
   <div class="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
@@ -75,7 +75,7 @@
 <div class="relative mt-8 bg-white">
   <div class="lg:absolute lg:inset-0">
     <div class="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
-      <img class="h-56 w-full object-cover lg:absolute lg:h-full" src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80" alt="">
+      <img class="h-56 w-full object-cover lg:absolute lg:h-full" src="images/LAMB-Site-Image-Services.jpg" alt="">
     </div>
   </div>
   <div class="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
