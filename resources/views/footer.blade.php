47796f8e0f345d8f0bc703d5bbf77d11728f6a87
= <dreadlord12318@gmail.com>
2022-05-11 09:32:49 +0800

    Added Content to Services Page

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -15,6 +15,12 @@
       PRICING
         </a>
       </div>
+
+      <div class="px-5 py-2">
+        <a href="/services" class="text-base text-slate-50 uppercase font-concoursec6 text-center font-normal leading-normal tracking-wide">
+      SERVICES
+        </a>
+      </div>
       <!-- <div class="px-5 py-2">
         <a href="/illuminations" class="text-base leading-normal font-normal text-center tracking-wide uppercase font-concoursec6 text-newcolor-700 hover:text-white">
        ILLUMINATIONS
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -15,12 +15,12 @@
     <div class="absolute inset-0 h-full bg-blue-700 mix-blend-multiply" aria-hidden="true"></div>
   </div>
   <div class="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
-    <h1 class="text-6xl text-left  font-concourset6  leading-tight font-normal tracking-normal text-newcolor-700 capitalize">We’re Ready</h1>
-    <p class="mt-6 font-copy text-2xl font-normal tracking-normal leading-normal text-left text-newcolor-700 max-w-3xl">If you’re an entrepreneur, a creator, a leader: you have a dream, a goal. Whatever you’re building, we’re here, ready and waiting to stand shoulder-to-shoulder with you. You bring the inspiration to do something unique, relevant, new, special. We’ll bring the process chops and the team.</p>
+    <h1 class="text-6xl text-left  font-concourset6  leading-tight font-normal tracking-normal text-newcolor-700 capitalize">Lambent Outsourcing Services</h1>
+    <p class="mt-6 font-copy text-2xl font-normal tracking-normal leading-normal text-left text-newcolor-700 max-w-3xl">Whether you're looking for long-term employees or need some extra help on a project basis, we've got you covered. </p>
   </div>
 </div>
 
-
+<div class="bg-white">
 <!-- Centered -->
 <div class="relative py-16 bg-white overflow-hidden">
   <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
@@ -54,39 +54,25 @@
   <div class="relative px-4 sm:px-6 lg:px-8">
     <div class="text-lg max-w-prose mx-auto">
       <h1>
-        <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">Introducing</span>
-        <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">JavaScript for Beginners</span>
+        <!-- <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">Our Service Model</span> -->
+        <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Our Service Model</span>
       </h1>
-      <p class="mt-8 text-xl text-gray-500 leading-8">Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget. Eleifend egestas fringilla sapien.</p>
+      <p class="mt-8 text-xl text-gray-500 leading-8">We deliver offshore high-value outsourcing solutions from the Philippines. We understand that your business success lies in how well you execute, and our job is to give you staffing options at reasonable prices and manage with your business goals in mind. </p>
+      <p class="mt-8 text-xl text-gray-500 leading-8">At Lambent Outsourcing, we know that finding the right talent for your business can be difficult. We solve staffing challenges with solid answers to grow your business with dependable, quality offshore individuals, teams, and direct hires. </p>
     </div>
     <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-      <p>Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong> sed <strong>eget risus enim</strong>. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id.</p>
-      <ul role="list">
-        <li>Quis elit egestas venenatis mattis dignissim.</li>
-        <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-        <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-      </ul>
-      <p>Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed elit interdum dignissim.</p>
-      <h2>From beginner to expert in 30 days</h2>
-      <p>Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.</p>
-      <blockquote>
-        <p>Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.</p>
-      </blockquote>
-      <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.</p>
-      <figure>
-        <img class="w-full rounded-lg" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3" alt="" width="1310" height="873">
-        <figcaption>Sagittis scelerisque nulla cursus in enim consectetur quam.</figcaption>
-      </figure>
-      <h2>Everything you need to get up and running</h2>
-      <p>Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>. Amet, massa quam varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus non molestie. Gravida quis fringilla amet eget dui tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat ac. Cras fermentum convallis quam.</p>
-      <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.</p>
+      <p class = "mt-8 text-xl text-gray-500 leading-8">We offer three options:</p>
+      <p class = "mt-8 text-xl text-gray-500 leading-8"><strong>Managed Virtual Assistants</strong> for creators, drop shippers, solopreneurs, and micro-businesses.</p>
+      <p class = "mt-8 text-xl text-gray-500 leading-8"><strong>Remote Teams</strong> for entrepreneurs and small businesses that require managed, dedicated teams for customer support, marketing, or back-office services.</p>
+      <p  class = "mt-8 text-xl text-gray-500 leading-8"><strong>Offshore Staffing</strong> for businesses that want to hire and manage their team directly.</p>
+      
     </div>
   </div>
 </div>
 
 <!-- Split with Image -->
 
-<div class="relative bg-white">
+<div class="relative mt-8 bg-white">
   <div class="lg:absolute lg:inset-0">
     <div class="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
       <img class="h-56 w-full object-cover lg:absolute lg:h-full" src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80" alt="">
@@ -95,28 +81,23 @@
   <div class="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
     <div class="lg:col-start-2 lg:pl-8">
       <div class="text-base max-w-prose mx-auto lg:max-w-lg lg:ml-auto lg:mr-0">
-        <h2 class="leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Work with us</h2>
-        <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Our Process</h3>
-        <p class="mt-8 text-lg text-gray-500">Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.</p>
-        <div class="mt-5 prose prose-indigo text-gray-500">
-          <p>Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.</p>
-          <p>Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique sit nisi lorem pulvinar. Urna, laoreet fusce nibh leo. Dictum et et et sit. Faucibus sed non gravida lectus dignissim imperdiet a.</p>
-          <p>Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus, egestas netus cursus. In.</p>
-          <ul role="list">
-            <li>Quis elit egestas venenatis mattis dignissim.</li>
-            <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-            <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-          </ul>
-          <p>Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.</p>
-          <h3>How we’re different</h3>
-          <p>Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam. Montes, magna cursus nulla feugiat dignissim id lobortis amet. Laoreet sem est phasellus eu proin massa, lectus. Diam rutrum posuere donec ultricies non morbi. Mi a platea auctor mi.</p>
-          <p>Mauris ullamcorper imperdiet nec egestas mi quis quam ante vulputate. Vel faucibus adipiscing lacus, eget. Nunc fermentum id tellus donec. Ut metus odio sit sit varius non nunc orci. Eu, mi neque, ornare suspendisse amet, nibh. Facilisi volutpat lectus id sapien dis mauris rhoncus. Est rhoncus, interdum imperdiet ac eros, diam mauris, tortor. Risus id sit molestie magna.</p>
-        </div>
+        <!-- <h2 class="leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Work with us</h2> -->
+        <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Managed Virtual Assistants</h3>
+        <p class="mt-4 text-lg text-gray-500">Lambent's Managed Virtual Assistants work as a pool managed by your personal concierge, who handles weekly calibration calls with you to coordinate tasks and assess the quality of your deliverables.</p>
+        <p class="mt-8 text-lg text-gray-500">This approach means you work with an experienced offshoring leader who will ensure your work is delivered correctly so you can focus on your work and not the delegation and management process. </p>
+        
+        <h3 class="mt-4 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Remote Teams</h3>
+        <p class="mt-4 text-lg text-gray-500">If you require more than one person to focus on a single process or a set of related functions, like help desk and inbound customer support, Lambent staffs, trains, and manages your offshore team for long-term success and high performance.</p>
+        <p class="mt-8 text-lg text-gray-500">Program teams handle a range of services, from data entry to graphic design to content moderation. Remote Teams are an ideal solution for teams of 2-20 personnel. </p>
+
+        <h3 class="mt-4 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Staffing Solutions</h3>
+        <p class="mt-4 text-lg text-gray-500">Lambent's time-tested techniques for staffing deliver quality professionals for businesses that want qualified direct hires at a 70% savings over their counterparts in Australia, Singapore, the United Kingdom, or the United States. </p>
+        <p class="mt-8 text-lg text-gray-500">Our team has an extensive, current database of active professionals who focus on marketing, business development, and customer support. Depending on your requirement, our local network can help you locate and assess local candidates using insights into Filipino culture and the local labor market. </p>
       </div>
     </div>
   </div>
 </div>
-
+</div>
 @include('footer')
 
 @endsection
