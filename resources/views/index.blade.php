f84823019965181470822123122e49644e7053f4
= <dreadlord12318@gmail.com>
2022-05-17 15:22:43 +0800

    Added Logo Cloud

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -506,46 +506,94 @@ important things.')
             </div>
         </div>
     </div>
+</div>
 
-    <!-- This example requires Tailwind CSS v2.0+ -->
+
+<!-- This example requires Tailwind CSS v2.0+ -->
+
+
+
+
+<!-- This example requires Tailwind CSS v2.0+ -->
 <div class="bg-white">
-  <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-    <div class="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
-      <div>
-        <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">Used by the world's most average companies</h2>
-        <p class="mt-3 max-w-3xl text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue.</p>
-        <div class="mt-8 sm:flex">
-          <div class="rounded-md shadow">
-            <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"> Create Account </a>
-          </div>
-          <div class="mt-3 sm:mt-0 sm:ml-3">
-            <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200"> Contact Us </a>
-          </div>
-        </div>
+  <div class="max-w-9xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+    <div class="grid grid-cols-5 gap-4 md:grid-cols-6 lg:grid-cols-5">
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Laravel.svg" alt="Laravel">
       </div>
-      <div class="mt-8 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
-        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-          <img class="max-h-12" src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg" alt="Workcation">
-        </div>
-        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-          <img class="max-h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage">
-        </div>
-        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-          <img class="max-h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple">
-        </div>
-        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-          <img class="max-h-12" src="https://tailwindui.com/img/logos/laravel-logo-gray-400.svg" alt="Laravel">
-        </div>
-        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-          <img class="max-h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit">
-        </div>
-        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-          <img class="max-h-12" src="https://tailwindui.com/img/logos/statamic-logo-gray-400.svg" alt="Statamic">
-        </div>
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Loom.svg" alt="Loom">
+      </div>
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Microsoft 365.svg" alt="Microsoft 365">
+      </div>
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Social Pilot.svg" alt="Social Pilot">
+      </div>
+      <div class="col-span-2 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-8" src="/images/Snovio.svg" alt="Snovio">
       </div>
     </div>
   </div>
 </div>
+<div class="bg-white">
+  <div class="max-w-9xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+    <div class="grid grid-cols-5 gap-4 md:grid-cols-6 lg:grid-cols-5">
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Toky.svg" alt="Toky">
+      </div>
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Tailwind CSS.svg" alt="Tailwind CSS">
+      </div>
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-20" src="/images/LinkedIn Sales Navigator.svg" alt="LinkedIn Sales Navigator">
+      </div>
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Ghost Org.svg" alt="Ghost Org">
+      </div>
+      <div class="col-span-2 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Figma.svg" alt="Figma">
+      </div>
+    </div>
+  </div>
+</div>
+<div class="bg-white">
+  <div class="max-w-9xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+    <div class="grid grid-cols-5 gap-4 md:grid-cols-6 lg:grid-cols-5">
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Miro.svg" alt="Miro">
+      </div>
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/ahrefs.svg" alt="Ahrefs">
+      </div>
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Grammarly.svg" alt="Grammarly">
+      </div>
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Canva.svg" alt="Canva">
+      </div>
+      <div class="col-span-2 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Clickup.svg" alt="Clickup">
+      </div>
+    </div>
+  </div>
+</div>
+<div class="bg-white">
+  <div class="max-w-9xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+    <div class="grid grid-cols-5 gap-4 md:grid-cols-6 lg:grid-cols-5">
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Plausible.svg" alt="Plausible">
+      </div>
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Jasper.svg" alt="Jasper">
+      </div>
+      <div class="col-span-1 flex justify-center md:col-span-1 lg:col-span-1">
+        <img class="h-12" src="/images/Google Apps.svg" alt="Google Apps">
+      </div>
+      
+  </div>
+</div>
+
 
 
     @include('footer')
