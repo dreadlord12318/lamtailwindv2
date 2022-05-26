a20802b4d583c710cf48cfdbf3ba62570c4635ef
= <dreadlord12318@gmail.com>
2022-05-26 10:03:00 +0800

    Fix Copy Spec

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -59,38 +59,17 @@ as well. ')
 <div class="relative py-16 bg-white overflow-hidden">
     <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
         <div class="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
-            <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none"
-                viewBox="0 0 404 384">
-                <defs>
-                    <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20"
-                        patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                    </pattern>
-                </defs>
-                <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
-            </svg>
-            <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404" height="384"
-                fill="none" viewBox="0 0 404 384">
-                <defs>
-                    <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20"
-                        patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                    </pattern>
-                </defs>
-                <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
-            </svg>
-            <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none"
-                viewBox="0 0 404 384">
-                <defs>
-                    <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20"
-                        patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                    </pattern>
-                </defs>
-                <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
-            </svg>
+           
+        </div>
+        <div class="text-lg max-w-prose mx-auto">
+         
         </div>
     </div>
+    <h1>
+
+<span
+    class="mt-2 block font-concourset6 text-5xl font-normal tracking-normal leading-tight text-center text-slate-900 capitalize ">Privacy Policy</span>
+</h1>
     <div class="relative px-4 sm:px-6 lg:px-8">
         <div class="text-lg max-w-prose mx-auto">
             <h1>
@@ -140,7 +119,7 @@ as well. ')
 
         </div>
     </div>
-
+</div>
     @include('footer')
 
 
--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -41,36 +41,7 @@ and terms.')
 <div class="relative py-16 bg-white overflow-hidden">
     <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
         <div class="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
-            <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none"
-                viewBox="0 0 404 384">
-                <defs>
-                    <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20"
-                        patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                    </pattern>
-                </defs>
-                <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
-            </svg>
-            <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404" height="384"
-                fill="none" viewBox="0 0 404 384">
-                <defs>
-                    <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20"
-                        patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                    </pattern>
-                </defs>
-                <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
-            </svg>
-            <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none"
-                viewBox="0 0 404 384">
-                <defs>
-                    <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20"
-                        patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                    </pattern>
-                </defs>
-                <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
-            </svg>
+         
         </div>
     </div>
     <div class="relative px-4 sm:px-6 lg:px-8">
@@ -78,7 +49,7 @@ and terms.')
             <h1>
 
                 <span
-                    class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Web
+                    class="mt-2 block font-concourset6 text-5xl font-normal tracking-normal leading-tight text-center text-slate-900 capitalize">Web
                     Site Terms</span>
             </h1>
         </div>
@@ -126,7 +97,7 @@ and terms.')
         <div class="text-lg max-w-prose mx-auto">
             <h1>
                 <span
-                    class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Business
+                    class="mt-2 block font-concourset6 text-5xl font-normal tracking-normal leading-tight text-center text-slate-900 capitalize ">Business
                     Process Outsourcing Services Terms</span>
             </h1>
         </div>
@@ -147,7 +118,7 @@ and terms.')
         <div class="text-lg max-w-prose mx-auto">
             <h1>
                 <span
-                    class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Compensation</span>
+                    class="mt-2 block font-concourset6 text-4xl font-normal tracking-normal leading-tight text-center text-slate-900 capitalize ">Compensation</span>
             </h1>
         </div>
         <div
@@ -315,7 +286,7 @@ and terms.')
         <div class="text-lg max-w-prose mx-auto">
             <h1>
                 <span
-                    class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Termination</span>
+                    class="mt-2 block font-concourset6 text-4xl font-normal tracking-normal leading-tight text-center text-slate-900 capitalize ">Termination</span>
             </h1>
         </div>
         <div
@@ -330,7 +301,7 @@ and terms.')
         <div class="text-lg max-w-prose mx-auto">
             <h1>
                 <span
-                    class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">General
+                    class="mt-2 block font-concourset6 text-4xl font-normal tracking-normal leading-tight text-center text-slate-900 capitalize ">General
                     Terms</span>
             </h1>
         </div>
